23207ef510862ee25782d099faf8f88fb15abcec
pranaavsk <pranaavkumar0690@gmail.com>
2026-08-15 21:35:33 +0530

    chapter 1 lab program

--- app/LabProgram.java
+++ app/LabProgram.java
@@ -1,4 +1,20 @@
 package app;
 
 public class LabProgram {
+    public static void main(String [] args)
+    {
+        int a=10;
+        long b=2000000000000000000l;
+        double c =200000000000000000000.0000000;
+        float d=20000000000000000000000000.0000000000000000000000000000f;
+        boolean e=true;
+        char f='i';
+        final double TAX_RATE = 0.18;
+        double amount = 1000.0;
+        System.out.println((int)amount);
+        int g=5/2;
+        int h=5/2;
+        System.out.println(g);
+        System.out.println(h);
+    }
 }
