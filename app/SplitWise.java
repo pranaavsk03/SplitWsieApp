43080dfec5e35da8f8e6daf2d27531354a617549
pranaavsk <pranaavkumar0690@gmail.com>
2026-08-20 20:06:42 +0530

    chapter 2 assigment

--- app/SplitWise.java
+++ app/SplitWise.java
@@ -1,5 +1,5 @@
 package app;
-
+import java.util.Scanner;
 public class SplitWise
 {
     public static void main(String[]args)
@@ -8,12 +8,37 @@ public class SplitWise
         double TOTAL_AMOUNT = 1200.0;
         int NUMBER_OF_PEOPLE = 4;
         System.out.println("=== Splitwise Console Backend ===");
-        double individualShare = TOTAL_AMOUNT / NUMBER_OF_PEOPLE;
-        System.out.println("Expense description: " + EXPENSE_DESCRIPTION);
-        System.out.println("Total amount: " + TOTAL_AMOUNT);
-        System.out.println("Number of people: " + NUMBER_OF_PEOPLE);
-        System.out.println("Each person should pay: " + individualShare);
-        System.out.println();
-        System.out.println("Feature status: Basic calculation completed.");
+        Scanner sc=new Scanner(System.in);
+        while(true)
+        {
+            System.out.println("1-Add expense");
+            System.out.println("2-View expense");
+            System.out.println("3-Exit");
+             int choice=sc.nextInt();
+             if(choice==1)
+            {
+                System.out.println("Expense was recording");
+            }
+             else if(choice==2)
+             {
+                 double individualShare = TOTAL_AMOUNT / NUMBER_OF_PEOPLE;
+                 System.out.println("Expense description: " + EXPENSE_DESCRIPTION);
+                 System.out.println("Total amount: " + TOTAL_AMOUNT);
+                 System.out.println("Number of people: " + NUMBER_OF_PEOPLE);
+                 System.out.println("Each person should pay: " + individualShare);
+                 System.out.println();
+                 System.out.println("Feature status: Basic calculation completed.");
+             }
+             else if(choice==3)
+             {
+                 break;
+             }
+             else {
+                 System.out.println("Input in invalid");
+             }
+        }
+        System.out.println( "Goodbye!");
+        sc.close();
+
     }
 }
